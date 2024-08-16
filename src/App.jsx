029dd33eb4138372768afbcff7c4545ede9301ac
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-08-17 01:02:46 +0300

    Implement image status

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import './App.css'
-import ImageSearch from './ImageSearch'
+import "./App.css";
+import { useState } from "react";
+import ImageSearch from "./ImageSearch";
 
 function App() {
+  const [imagesStatus, setImagesStatus] = useState([]);
+
+  const handleLikes = (id) => {
+    setImagesStatus((prevImagesStatus) =>
+      prevImagesStatus.map((image) =>
+        image.id === id ? { ...image, status: image.status + 1 } : image
+      )
+    );
+  };
+
+  const handleDislikes = (id) => {
+    setImagesStatus((prevImagesStatus) =>
+      prevImagesStatus.map((image) =>
+        image.id === id ? { ...image, status: image.status - 1 } : image
+      )
+    );
+  };
+
+  const addImages = (images) => {
+    const imagesWithStatus = images.map((image) => ({
+      id: image.id,
+      status: 0,
+    }));
+    setImagesStatus(imagesWithStatus);
+  };
 
   return (
     <>
-     <ImageSearch />
+      <ImageSearch
+        imagesStatus={imagesStatus}
+        handleLikes={handleLikes}
+        handleDislikes={handleDislikes}
+        addImages={addImages}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
