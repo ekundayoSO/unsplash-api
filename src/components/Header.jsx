07cbbcc6b8aebe5ef51eb0633d8e75de71a4f194
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-08-18 02:05:00 +0300

    Update Header component with toggle css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div>
+    <div className="relative">
       <nav className="bg-black text-white py-4 fixed right-0 left-0 top-0 pb-4">
         <h1 className="text-left ml-2 font">Ekundayo Sulaimon</h1>
       </nav>
