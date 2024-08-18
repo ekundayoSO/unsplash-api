c04738bb24c3a2263caca0c00a3bfa9575ae0868
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-08-18 04:18:53 +0300

    Fix reduntant statements in toggle component

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from'react';
-import { FaToggleOff } from "react-icons/fa";
-
+import { MdOutlineDarkMode } from "react-icons/md";
 
 const ToggleButton = () => {
-  const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem('theme') === 'dark';
-  });
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
     if (darkMode) {
@@ -22,7 +19,7 @@ const ToggleButton = () => {
       onClick={() => setDarkMode(!darkMode)}
       className="p-0.5 bg-gray-400 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded fixed top-3 right-0"
     >
-        <FaToggleOff color='white'/>
+        <MdOutlineDarkMode />
     </button>
   );
 };
